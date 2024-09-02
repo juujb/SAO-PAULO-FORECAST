2af122ec828c4b69212b50ce3fbd694fcc2229de
Jucélio Brandão <juceneon@gmail.com>
2024-09-01 23:00:03 -0300

    Design conforme figma da tela de seleção de localidade.

--- app/src/main/java/com/example/fiapclimatempo/components/LocationButton.kt
+++ app/src/main/java/com/example/fiapclimatempo/components/LocationButton.kt
@@ -4,25 +4,32 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
-fun LocationButton(modifier: Modifier = Modifier, text: String, lat: String, long: String, navController: NavController) {
+fun LocationButton(text: String, lat: String, long: String, navController: NavController) {
     Button(
         onClick = { navController.navigate("climate-forecast/$text?lat=$lat&long=$long") }, // Para quem for utilizar lat e long na chamada da API, manipular o navigate de acordo
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.White)
     ) {
         Text(
             text,
-            style = MaterialTheme.typography.labelLarge,
-            fontSize = 20.sp
+            style = MaterialTheme.typography.labelLarge.copy(
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            color = Color.Black,
         )
     }
-    Spacer(modifier = Modifier.height(4.dp))
+    Spacer(modifier = Modifier.height(16.dp))
 }
--- app/src/main/java/com/example/fiapclimatempo/screens/LocationSelector.kt
+++ app/src/main/java/com/example/fiapclimatempo/screens/LocationSelector.kt
@@ -1,16 +1,20 @@
 package com.example.fiapclimatempo.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -22,53 +26,66 @@ fun LocationSelection(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 16.dp, bottom = 16.dp),
+            .background(color = Color(0xFF00BFFF)),
     ) {
-        AppHeader(
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .fillMaxWidth()
-        )
-        Text(
-            text = "Selecione a Localização:",
-            style = MaterialTheme.typography.labelLarge,
-            fontSize = 32.sp,
-            modifier = Modifier
-                .padding(top = 80.dp, bottom = 500.dp)
-                .align(Alignment.Center)
-        )
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(30.dp)
-                .align(Alignment.TopCenter),
+                .align(Alignment.TopCenter)
+                .padding(top = 16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AppHeader()
+        }
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(horizontal = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
-            LocationButton(
-                text = "São Paulo",
-                lat = "-23.5489",
-                long = "-46.6388",
-                navController = navController
+            Text(
+                text = "Selecione a localização:",
+                style = MaterialTheme.typography.labelMedium.copy(
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.ExtraBold
+                ),
+                modifier = Modifier
+                    .padding(top = 30.dp),
+                color = Color.White
             )
-            LocationButton(
-                text = "Diadema",
-                lat = "-23.6865",
-                long = "-46.6234",
-                navController = navController
-            )
-            LocationButton(
-                text = "São Bernardo do Campo",
-                lat = "-23.6944",
-                long = "-46.5654",
-                navController = navController
-            )
-            LocationButton(
-                text = "Santo André",
-                lat = "-23.6666",
-                long = "-46.5322",
-                navController = navController
-            )
+            Column(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(30.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                LocationButton(
+                    text = "São Paulo",
+                    lat = "-23.5489",
+                    long = "-46.6388",
+                    navController = navController
+                )
+                LocationButton(
+                    text = "Diadema",
+                    lat = "-23.6865",
+                    long = "-46.6234",
+                    navController = navController
+                )
+                LocationButton(
+                    text = "São Bernardo do Campo",
+                    lat = "-23.6944",
+                    long = "-46.5654",
+                    navController = navController
+                )
+                LocationButton(
+                    text = "Santo André",
+                    lat = "-23.6666",
+                    long = "-46.5322",
+                    navController = navController
+                )
+            }
         }
     }
 }
